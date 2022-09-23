afc072db1e5292c48379ea05b4bf8b8717f84531
superswaggySUP <91008102+superswaggySUP@users.noreply.github.com>
2022-09-23 16:00:18 +0800

    github commit

--- src/main/java/com/meizhou/git/gitTest.java
+++ src/main/java/com/meizhou/git/gitTest.java
@@ -10,5 +10,6 @@ public class gitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("听我说妈妈");
     }
 }
