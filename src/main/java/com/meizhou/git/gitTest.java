756eb573d570f7f7ec46e9326d3e01d16c94dda2
superswaggy <1805751978@qq.com>
2022-09-23 14:37:15 +0800

    second commit

--- src/main/java/com/meizhou/git/gitTest.java
+++ src/main/java/com/meizhou/git/gitTest.java
@@ -3,5 +3,9 @@ package com.meizhou.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello github");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
