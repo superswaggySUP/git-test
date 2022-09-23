48ca82cc4901bba148493cddfb842ded85b458a5
superswaggySUP <91008102+superswaggySUP@users.noreply.github.com>
2022-09-23 19:51:44 +0800

    Update gitTest.java

--- src/main/java/com/meizhou/git/gitTest.java
+++ src/main/java/com/meizhou/git/gitTest.java
@@ -11,5 +11,6 @@ public class gitTest {
         System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("听我说妈妈");
+        System.out.println("wobunenghuijia");
     }
 }
